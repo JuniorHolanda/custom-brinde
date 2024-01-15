e431f65920909595f058b0a98875caedbc109700
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-01-15 18:58:28 -0300

    inicio dos testes
    início dos testes para capturar o scroll e alterar a matiz da imagem e assim criar um animação nas cores enquanto o scroll rola

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,11 @@
-// quando mouse passar sobre botão, botão = destaque e mostrar balão com botão de cores
-// enquanto mouse estiver sobre o botão ou sobre o balão o mesmo deve permanecer na tela, e desaparecer quando o mouse sair de cima de um dos dois.
+/*  telaGira = captura valores do scroll
+    valor.mudaMatiz =  muda a matiz  -> filter: hue-rotate(deg)
+    
+    criar uma funcao que altera o hue-rotate de 0 a 360.
+     valor.mudaMatiz = telaGira
 
+*/
 
-
-
-const listaBtnDestaque = document.querySelectorAll('.listaBtnDestaque')
-const listaPartesProdutoBtn = document.querySelectorAll('.btnComponentesProduto');
-
-listaPartesProdutoBtn[0].addEventListener('mouseover', function (){
-    this.style.backgroundColor = '#fc5b5b';
-    listaBtnDestaque[0].classList.remove('listaBtnDestaque')
-
-})
-
-listaPartesProdutoBtn[0].addEventListener('mouseout' , function () {
-   
-    listaBtnDestaque[0].classList.add('listaBtnDestaque')
-    this.style.backgroundColor = '#d9d9d9';
+document.addEventListener('DOMContentLoaded' , function () {
+    
 })
