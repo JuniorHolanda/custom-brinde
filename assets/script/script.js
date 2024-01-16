2e4e2e4ea4da9a5153c6d2dff0e188da5988967e
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-01-15 22:32:10 -0300

    alteração imagens
    modifiquei a cor das imagens para sairem de posições diferentes no circulo de cores

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,22 +2,22 @@
     valor.mudaMatiz =  muda a matiz  -> filter: hue-rotate(deg)
     
     criar uma funcao que altera o hue-rotate de 0 a 360.
-     valor.mudaMatiz = telaGira
+    valor.mudaMatiz = telaGira
 
 */
- let scrollPosition = 0;
- let parteCorpo = document.querySelector('.mudaCorpo')
+let scrollPosition = 0;
+let parteCorpo = document.querySelector('.mudaCorpo')
+let parteTampa = document.querySelector('.mudaTampa')
+let parteDetalhes = document.querySelector('.mudaDetalhe')
 
- window.addEventListener('scroll', function () {
+window.addEventListener('scroll', function () {
     scrollPosition = this.window.scrollY;
-    parteCorpo.style.filter = 'hue-rotate(' + scrollPosition / 6 + 'deg)'
- })
-
- console.log('scrollPosition')
-
+    parteCorpo.style.filter = 'hue-rotate(' + scrollPosition / 4 + 'deg)'
+    parteTampa.style.filter = 'hue-rotate(' + scrollPosition / 4 + 'deg)'
+})
 
 
- /*
+/*
 let scrollPosition = 0;
 let parteCorpo = document.querySelector('.mudaCorpo');
 
@@ -27,4 +27,4 @@ window.addEventListener('scroll', function () {
 });
 
 console.log(scrollPosition);
- */
+*/
