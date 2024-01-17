d5c97cb529f6f1f3a5808d4edf0e4b86c790321c
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-01-17 03:25:25 -0300

    ainda tentando refatorar o código

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,21 +2,13 @@
 
 let scrollPosition = 0;
 let posicaoCaixaBtn = document.querySelector('.containerBTN')
-let caixaVisivel = document.querySelector('.caixaBtn')
+let caixaVisivel = document.querySelector('#caixaBtn')
 
-//----------------INÍCIO BOTÕES CORES-------------------
+//----------------INÍCIO BOTÕES--------------------------------------
+let listBtnDestaque = document.querySelectorAll('.btnDestaqueItem')
+let listBtnCor = document.querySelector('.btnCor');
+//----------------FIM BOTÕES-----------------------------------------
 
-let btnVermelho = document.querySelector('.btnVermelho');
-let btnAzul = document.querySelector('.btnAzul');
-let btnPreto = document.querySelector('.btnPreto');
-
-//----------------INÍCIO BOTÕES DESTAQUE----------------
-
-let btnCorpo = document.querySelector('#btnCorpo');
-let btnTampa = document.querySelector('#btnTampa');
-let btnDetalhe = document.querySelector('#btnDetalhe');
-
-//--------------INÍCIO SOBREPOSICAO IMAGENS-------------
 
 let parteCorpo = document.querySelector('.mudaCorpo')
 let parteTampa = document.querySelector('.mudaTampa')
@@ -24,14 +16,88 @@ let parteDetalhes = document.querySelector('.mudaDetalhe')
 
 
 
-
-
 window.addEventListener('scroll', function () {
     scrollPosition = this.window.scrollY;
-    parteCorpo.style.filter = 'hue-rotate(' + scrollPosition / 4 + 'deg)'
-    parteTampa.style.filter = 'hue-rotate(' + scrollPosition / 3.8 + 'deg)'
+    parteCorpo.style.filter = 'hue-rotate(' + scrollPosition / 4 + 'deg)';
+    parteTampa.style.filter = 'hue-rotate(' + scrollPosition / 3.8 + 'deg)';
+})
+
+
+
+   
+
+
+listBtnDestaque[0].addEventListener('mouseover' , function () {
+    caixaVisivel.classList.remove('some');
+    caixaVisivel.style.left = 0
+})
+
+listBtnDestaque[0].addEventListener('mouseout' , function () {
+    
+    caixaVisivel.classList.add('some');
     
 })
+
+listBtnDestaque[1].addEventListener('mouseover' , function () {
+    caixaVisivel.classList.remove('some');
+    caixaVisivel.style.left = '14.5rem'
+})
+
+listBtnDestaque[1].addEventListener('mouseout' , function () {
+    caixaVisivel.classList.add('some');
+})
+
+listBtnDestaque[2].addEventListener('mouseover' , function () {
+    caixaVisivel.classList.remove('some');
+    caixaVisivel.style.left = '29.5rem'
+})
+
+listBtnDestaque[2].addEventListener('mouseout' , function () {
+    caixaVisivel.classList.add('some');
+})
+
+
+
+
+/*
+
+function position (btnClicado) {
+    
+    posicaoCaixaBtn.style.alignItems = btnClicado
+    
+    if (posicaoCaixaBtn.style.alignItems === btnClicado) {
+        caixaVisivel.style.opacity = 1
+    } else if (posicaoCaixaBtn.mouseover != true) {
+        caixaVisivel.style.opacity = 0
+    }
+}
+
+listBtnDestaque[0].addEventListener('click', function () {
+    position ('flex-start')
+})
+
+listBtnDestaque[1].addEventListener('click', function () {
+    position ('center')
+})
+
+listBtnDestaque[2].addEventListener('click', function () {
+    position ('flex-end')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function moveCaixa (posicao) {
@@ -40,72 +106,28 @@ function moveCaixa (posicao) {
 
 
 
+// inicio botao detalhe
+
 btnCorpo.addEventListener('mouseover', function () {
-    moveCaixa('flex-start')
+    moveCaixa('flex-start');
     caixaVisivel.style.opacity = '100%';
 });
 
 btnCorpo.addEventListener('mouseout', function () {
+    moveCaixa('center');
     caixaVisivel.style.opacity = '0';
 });
 
-
-
-
-
-btnCorpo.addEventListener('mouseout', function () {
-    moveCaixa('center')
-});
+// inicio botao detalhe
 
 btnDetalhe.addEventListener('mouseover' , function () {
-    moveCaixa ('flex-end')
+    moveCaixa ('flex-end');
+    caixaVisivel.style.opacity = '100%';
 })
 
 btnDetalhe.addEventListener('mouseout' , function () {
-    moveCaixa ('center')
+    moveCaixa ('center');
+    caixaVisivel.style.opacity = '0';
 })
-
-
-
-/*
-let scrollPosition = 0;
-let parteCorpo = document.querySelector('.mudaCorpo');
-
-window.addEventListener('scroll', function () {
-    scrollPosition = window.scrollY;
-    
-});
-
-console.log(scrollPosition);
-*/
-
-
-
-/*
-temos as 3 partes referenciadas
-preciso dos 3 botoes referenciado (Não sei se en lista ou individualmente)
-preciso de um event listener que pegue o btn a cor escolhida e mude a cor da parte
-
-
-havera apenas 1 div com os botões
-está div deve responder de de forma dinamica ***PARAMETROS*** 
-
-eu tenho 3 botões
-cada 1 deles passa o mesmo comando para imagens diferentes
-o que deve mudar é o parametro
-
-
-let btnCorpo = document.querySelector('.btnVermelho');
-let btnCorpo = document.querySelector('.btnAzul');
-let btnCorpo = document.querySelector('.btnPreto');
-
-
-
-function (frase) {
-    frase.style.filter = 'hue-rotate('4deg)'
-})
-}
-
-
 
 */
