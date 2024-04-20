9469a238809369b27c63ced35889577ab2dae9b2
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-04-20 13:34:19 -0300

    depois de muito tempo, voltei aqui
    faz tem que mexi aqui da ultima vez, dessa vez estou com mais conhecimento acumulado e vou tentar novamente

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,7 +1,5 @@
 // botões principais
-const btnDoCorpo = document.querySelector('#btnCorpo');
-const btnDoDetalhes = document.querySelector('#btnDetalhes');
-const btnDoTampa = document.querySelector('#btnTampa');
+const listaBtnMain = document.querySelectorAll('.btnPrincipais')
 
 // referencia o container pai dos dois tipos de botões, principal e secundários
 const containerPaiDosBotoes = document.querySelector('#controlePrincipal');
@@ -13,43 +11,14 @@ containerBtnSecondarios.classList.add('btnSecundariosContainer');
 //aray com as cores dos botões secundários
 const cores = ['Azul', 'Vermelho', 'Preto'];
 
-let botaoAtivo = null; //armazena o último botão principal clicado
-
-
-function criarBotoesSecundarios(botaoPrincipal) {
-    if (botaoPrincipal !== botaoAtivo) {
-        // Se o botão principal clicado for diferente do último botão ativo
-        if (botaoAtivo !== null) {
-            // Se houver botões secundários ativos, remova-os
-            containerPaiDosBotoes.removeChild(containerBtnSecondarios);
-        }
-
-        // Crie os botões secundários apenas se o contêiner estiver vazio
-        if (containerBtnSecondarios.children.length === 0) {
-            cores.forEach(cor => {
-                const btnSecundario = document.createElement('button');
-                btnSecundario.classList.add('btnSecundarios', `btnSecundarios${cor}`);
-                containerBtnSecondarios.appendChild(btnSecundario);
-            });
-
-            // Adicione a caixa de botões secundários ao contêiner pai
-            containerPaiDosBotoes.appendChild(containerBtnSecondarios);
-        }
-
-        // Atualize o botão ativo
-        botaoAtivo = botaoPrincipal;
+for (i=0; i < listaBtnMain.length; i++){
+    
+    function mostraId (id) {
+        console.log(id)
     }
 }
 
 
-
-// Adiciona ouvintes de eventos para os botões principais
-btnDoCorpo.addEventListener('click', function() {
-    criarBotoesSecundarios(btnDoCorpo);
-});
-btnDoDetalhes.addEventListener('click', function() {
-    criarBotoesSecundarios(btnDoDetalhes);
-});
-btnDoTampa.addEventListener('click', function() {
-    criarBotoesSecundarios(btnDoTampa);
+listaBtnMain.addEventListener('click', function() {
+    mostraId()
 });
