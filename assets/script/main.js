2aa5d0f3c94e920ad5b1fd858862c9da3607ba89
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-04-21 19:44:03 -0300

    Agora começa a brincadeira
    tudo certo, função abre a caixa de modal com 3 botões e agora vou ligar os botões a cada uma das partes

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,38 +4,47 @@ const listaBtnMain = document.querySelectorAll('.btnPrincipais')
 // referencia o container pai dos dois tipos de botões, principal e secundários
 const containerPaiDosBotoes = document.querySelector('#controlePrincipal');
 
-//cria e adiciona classe a uma div para a lista de botões secundários
-const containerBtnSecondarios = document.createElement('div');
 
-const nomeDasCores = [ 'azul' , 'vermelho' , 'preto' ]
-const btnCoresModal = ['button' , 'button' , 'button'];
+//----------------------- Cria os botões de cores e atribuem classe -----------------------------
 
-function btnMudaCor (cor) {
-    btnCoresModal[i].classList.add('btnCores');
-    btnCoresModal[i].id = cor;
-}
-
-// laço para a criação dos botões de cores cores
-for (let i = 0; i < btnCoresModal.length; i++) {
-    console.log(btnCoresModal.id);
-    btnMudaCor(nomeDasCores[i])
-}
-
-let titleModal = document.createElement('h3');
-titleModal.textContent = 'Escolha a cor do corpo';
-
-containerBtnSecondarios.classList.add('btnSecundariosContainer');
+const nomeDasCores = [ 'Azul' , 'Vermelho' , 'Preto' ];
+const btnCor = [];
 
 let modalBtnSecundario = null;
+let btnModal = null;
 //função recebe como argumento o id da lista listaBtnMain 
 function mostraBotao (id) {
     if (modalBtnSecundario){
         modalBtnSecundario.remove();
         modalBtnSecundario = null;
+        btnModal = null;
+
     } else {
         modalBtnSecundario = document.createElement('div');
         modalBtnSecundario.classList.add('containerModalBtnSecundario');
         containerPaiDosBotoes.appendChild(modalBtnSecundario);
+
+        // laço para a criação dos botões de cores cores
+
+        for (let i = 0; i < nomeDasCores.length; i++) {
+            // Verifica se o botão de cor já foi criado antes
+            let corJaAdicionada = btnCor.some(btn => btn.classList.contains(`btn${nomeDasCores[i]}`));
+            // se o resultado for diferente de contem:
+            if (!corJaAdicionada) {
+                btnModal = document.createElement('button');
+                btnModal.classList.add(`btn${nomeDasCores[i]}`, 'btnSecundario');
+                btnModal.textContent = nomeDasCores[i];
+                modalBtnSecundario.appendChild(btnModal);
+                btnCor.push(btnModal);
+            } else {
+                // Se o resultado é diferente de não conter então cria os botões mas não adiciona mais à lista
+                btnModal = document.createElement('button');
+                btnModal.classList.add(`btn${nomeDasCores[i]}`, 'btnSecundario');
+                btnModal.textContent = nomeDasCores[i];
+                modalBtnSecundario.appendChild(btnModal);
+            }
+        
+        }
     }
        
 }
