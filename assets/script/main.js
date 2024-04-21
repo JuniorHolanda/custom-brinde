b3971e8bd63a52593b32280b7a650f3fff359896
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-04-21 11:29:59 -0300

    função muda cor

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,19 +6,47 @@ const containerPaiDosBotoes = document.querySelector('#controlePrincipal');
 
 //cria e adiciona classe a uma div para a lista de botões secundários
 const containerBtnSecondarios = document.createElement('div');
-containerBtnSecondarios.classList.add('btnSecundariosContainer');
 
-//aray com as cores dos botões secundários
-const cores = ['Azul', 'Vermelho', 'Preto'];
+const nomeDasCores = [ 'azul' , 'vermelho' , 'preto' ]
+const btnCoresModal = ['button' , 'button' , 'button'];
 
-for (i=0; i < listaBtnMain.length; i++){
-    
-    function mostraId (id) {
-        console.log(id)
-    }
+function btnMudaCor (cor) {
+    btnCoresModal[i].classList.add('btnCores');
+    btnCoresModal[i].id = cor;
 }
 
+// laço para a criação dos botões de cores cores
+for (let i = 0; i < btnCoresModal.length; i++) {
+    console.log(btnCoresModal.id);
+    btnMudaCor(nomeDasCores[i])
+}
 
-listaBtnMain.addEventListener('click', function() {
-    mostraId()
-});
+let titleModal = document.createElement('h3');
+titleModal.textContent = 'Escolha a cor do corpo';
+
+containerBtnSecondarios.classList.add('btnSecundariosContainer');
+
+let modalBtnSecundario = null;
+//função recebe como argumento o id da lista listaBtnMain 
+function mostraBotao (id) {
+    if (modalBtnSecundario){
+        modalBtnSecundario.remove();
+        modalBtnSecundario = null;
+    } else {
+        modalBtnSecundario = document.createElement('div');
+        modalBtnSecundario.classList.add('containerModalBtnSecundario');
+        containerPaiDosBotoes.appendChild(modalBtnSecundario);
+    }
+       
+}
+
+// Estrutura de laço, enquanto i for menor que o total de itens da listaBtnMain o laço acontece, a cada laço é atribuído ao botão da vez um ouvinte de click.
+for (i=0; i < listaBtnMain.length; i++){
+    let botao = listaBtnMain[i];
+    botao.addEventListener('click' , function() {
+        //captura o id do elemento da vez no laço
+        let capturaId = this.id;
+        //passa-se o id capturado a armazenado como argumento para a função
+        mostraBotao (capturaId);
+    })
+}
