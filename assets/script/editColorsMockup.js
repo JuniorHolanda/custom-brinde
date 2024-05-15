af8d76d3b19ba0c285cab89e9e7844aeab60335c
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-05-14 22:14:01 -0300

    lógica funcionando lindo :alien:
    agora só precisa de ajustes, mas o pior ja passou UHULLLL

--- assets/script/editColorsMockup.js
+++ assets/script/editColorsMockup.js
@@ -1,19 +1,26 @@
 import { controlForProducts } from "./mockup.js";
 
-const listBtnColor = ['Vermelho' , 'Azul' , 'Amarelo']; 
+const listBtnColor = ['vermelho' , 'royal' , 'amarelo' , 'roxo' , 'preto']; 
 const containerBtnColors = document.createElement('div');
 containerBtnColors.classList.add('container-btn-colors')
 
 export function createBtnColor (imagem) {
-    const listImgPart = document.querySelectorAll('.img-mockup')
+    const listImgPart = document.querySelectorAll('.img-mockup');
+
+    // iteração sobre a lista com as partes do produto
 
     function changeColorPartProduct (color) {
+
+        //itera sobre a lista com partes do produto, captura seu .src em uma variável e compara se dentro dele está incluso o imagem
         for (let i = 0; i < listImgPart.length; i++){
-            const imgPart = listImgPart[i]
-            imgPart.classList.add(color)
-            console.log(imgPart)
+            const nameProduct = listImgPart[i].src
+            if (nameProduct.includes(imagem)){
+                listImgPart[i].classList.add(`color-part-${color}`)
+            }
         }
+
     }
+
 
     // itera sobre o listBtnColor e cria os botões de cores
     for (let i = 0; i < listBtnColor.length; i++){
