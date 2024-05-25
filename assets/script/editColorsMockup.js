dc6e11ffe45302416f481945d17ccd1df251906c
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-05-25 20:19:47 -0300

    melhorando o código com eslint e pretter

--- assets/script/editColorsMockup.js
+++ assets/script/editColorsMockup.js
@@ -1,43 +1,43 @@
 import { controlForProducts } from "./mockup.js";
 
-export let btnColor = '';
+export let btnColor = "";
 
 // Recebe as cores para os produtos
-const listBtnColor = ['vermelho' , 'royal' , 'amarelo' , 'roxo' , 'preto']; 
-export const containerBtnColors = document.createElement('div');
-containerBtnColors.classList.add('container-btn-colors')
+const listBtnColor = ["vermelho", "royal", "amarelo", "roxo", "preto"];
+export const containerBtnColors = document.createElement("div");
+containerBtnColors.classList.add("container-btn-colors");
 
-export function createBtnColor (imagem) {
-    const listImgPart = document.querySelectorAll('.img-mockup');
+export function createBtnColor(imagem) {
+  const listImgPart = document.querySelectorAll(".img-mockup");
 
-    // iteração sobre a lista com as partes do produto
-    function changeColorPartProduct (color) {
-        //itera sobre a lista com partes do produto, captura seu .src em uma variável e compara se dentro dele está incluso o imagem
-        for (let i = 0; i < listImgPart.length; i++){
-            const nameProduct = listImgPart[i].src // atribui a nameProduct o .src de cada item do listImgParty
-            
-            // verifica se dentro do nameProduct tem o nome do produto passado como parâmetro
-            if (nameProduct.includes(imagem)){
-                listImgPart[i].classList.add(`color-part-${color}`)
-            } // fim do if
-        } // fim do for
-    } //fim da função changeColorPartProduct
+  // iteração sobre a lista com as partes do produto
+  function changeColorPartProduct(color) {
+    //itera sobre a lista com partes do produto, captura seu .src em uma variável e compara se dentro dele está incluso o imagem
+    for (let i = 0; i < listImgPart.length; i++) {
+      const nameProduct = listImgPart[i].src; // atribui a nameProduct o .src de cada item do listImgParty
 
+      // verifica se dentro do nameProduct tem o nome do produto passado como parâmetro
+      if (nameProduct.includes(imagem)) {
+        listImgPart[i].classList.add(`color-part-${color}`);
+      } // fim do if
+    } // fim do for
+  } //fim da função changeColorPartProduct
 
-    if (btnColor !== '') {
-        btnColor.innerHTML = '';
-        containerBtnColors.innerHTML = ''
-    }
+  if (btnColor !== "") {
+    btnColor.innerHTML = "";
+    containerBtnColors.innerHTML = "";
+  }
 
-    // itera sobre o listBtnColor e cria os botões de cores
-    for (let i = 0; i < listBtnColor.length; i++){
-        btnColor = document.createElement('button');
-        btnColor.textContent = listBtnColor[i];
-        btnColor.classList.add(`btn-color-${listBtnColor[i]}` , 'btn-color');
-        containerBtnColors.appendChild(btnColor);
-        btnColor.addEventListener('click' , () => changeColorPartProduct (listBtnColor[i]));
-    }
+  // itera sobre o listBtnColor e cria os botões de cores
+  for (let i = 0; i < listBtnColor.length; i++) {
+    btnColor = document.createElement("button");
+    btnColor.textContent = listBtnColor[i];
+    btnColor.classList.add(`btn-color-${listBtnColor[i]}`, "btn-color");
+    containerBtnColors.appendChild(btnColor);
+    btnColor.addEventListener("click", () =>
+      changeColorPartProduct(listBtnColor[i])
+    );
+  }
 
-    controlForProducts.appendChild(containerBtnColors);
+  controlForProducts.appendChild(containerBtnColors);
 }
-
